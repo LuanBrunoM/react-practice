fc57577a60c525e021ee580cf97a56142a5213ff
Luan Bruno <luanbrunomelo03@gmail.com>
2023-07-18 20:58:17 -0300

    conceitos do jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
 import React from "react";
 
 const App = () => {
+  function meuNome() {
+    return 'André';
+  }
+
+  function quadrado(x) {
+    return x * x;
+  }
+
+  const carro = {
+    rodas: 4,
+    marca: 'Ford',
+  };
+
+  const estiloP = {
+    color: "blue",
+    fontSize: '4rem',
+  }
+
   return (
-      <div>
-        App React  <button>Adicionar</button>
-      </div>
-        
-  )
+    <div>
+      <p style={estiloP}>{'MINHA EMPRESA'.toLowerCase()}</p>
+      <p>{30 * 2}</p>
+      <p>{true}</p>
+      <p>{undefined}</p>
+      <p>{(() => 'Função Executada')()}</p>
+      <p>{meuNome()}</p>
+      <p>{quadrado(2)}</p>
+      <p>{quadrado(2) === 4 ? 'Fórmula correta' : 'Fórmula incorreta'}</p>
+      <p>{quadrado(2) === 4 && 'Fórmula correta'}</p>
+      <p>{Date.now()}</p>
+      <p>{new Date().getFullYear()}</p>
+      <p>
+        Marca: {carro.marca}, Rodas: {carro.rodas}
+      </p>
+    </div>
+  );
 }
 
 export default App
