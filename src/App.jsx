87b4320068a6369a6d37f12381ffaf2cf6889543
Luan Bruno <luanbrunomelo03@gmail.com>
2023-07-19 18:25:37 -0300

    exercicio JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,53 @@
+// Mostre os dados da aplicação, como aprensetado no vídeo
+// Não utilize CSS externo, use o style para mudar as cores
+// Se a situação estiver ativa pinte de verde, inativa vermelho
+// Se o gasto for maior que 10000 mostre uma mensagem
+
+
 import React from "react";
 
+
+const luana = {
+  cliente: 'Luana',
+  idade: 27,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+  ],
+  ativa: true,
+};
+
+
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+    { nome: 'Guitarra', preco: 'R$ 3500' },
+  ],
+  ativa: false,
+};
+
 const App = () => {
-  function meuNome() {
-    return 'André';
-  }
+  const dados = mario;
 
-  function quadrado(x) {
-    return x * x;
-  }
-
-  const carro = {
-    rodas: 4,
-    marca: 'Ford',
-  };
-
-  const estiloP = {
-    color: "blue",
-    fontSize: '4rem',
-  }
+  const total = dados.compras.map(item =>
+    +item.preco.replace('R$ ','')
+  ).reduce((a, b) => a + b);
 
   return (
-    <div>
-      <p style={estiloP}>{'MINHA EMPRESA'.toLowerCase()}</p>
-      <p>{30 * 2}</p>
-      <p>{true}</p>
-      <p>{undefined}</p>
-      <p>{(() => 'Função Executada')()}</p>
-      <p>{meuNome()}</p>
-      <p>{quadrado(2)}</p>
-      <p>{quadrado(2) === 4 ? 'Fórmula correta' : 'Fórmula incorreta'}</p>
-      <p>{quadrado(2) === 4 && 'Fórmula correta'}</p>
-      <p>{Date.now()}</p>
-      <p>{new Date().getFullYear()}</p>
-      <p>
-        Marca: {carro.marca}, Rodas: {carro.rodas}
-      </p>
-    </div>
+    <>
+      <p>Nome: {dados.cliente}</p>
+      <p>Idade: {dados.idade}</p>
+      <p>Situação: <span style={{color: dados.ativa ? 'green' : 'red'}}>{dados.ativa ? 'Ativa' : 'Inativa'}</span></p>
+      <p>Total de gastos: {total}</p>
+      {total > 10000 && <p>Você está gastando muito</p>}
+    </>
   );
-}
+};
+
 
 export default App
