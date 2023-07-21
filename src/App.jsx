6e6daee9ba94481e9a4b87decf807c9008fb34ae
Luan Bruno <luanbrunomelo03@gmail.com>
2023-07-21 13:12:58 -0300

    exercicio

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,23 @@ const produtos = [
 ];
 
 const App = () => {
-  return <section></section>;
+  const dados = produtos.filter(({preco}) => +preco.replace('R$ ', '') > 1500);
+  
+  return (
+    <section>
+      {dados.map(({id, nome, preco, cores}) => 
+        <div key={id}>
+          <h1>{nome}</h1>
+          <p>Preco: {preco}</p>
+          <ul>
+            {cores.map((cor) => (
+              <li style={{backgroundColor: cor, color: 'white'}} key={cor}>{cor}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </section>
+  );
 };
 
 export default App
