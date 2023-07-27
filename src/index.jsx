50f7410952e6c4e4dab406dcfd0e063944518597
Luan Bruno <luanbrunomelo03@gmail.com>
2023-07-26 22:28:20 -0300

    update

--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home.jsx';
 
-import JsxConceitos from './pages/1-jsx-conceitos/JsxConceitos.jsx'
-import JsxConceitosExercicios from './pages/1-jsx-conceitos/JsxConceitosExercicios.jsx'
-import JsxArrays from './pages/2-jsx-arrays/JsxArrays.jsx'
-import JsxArraysExercicio from './pages/2-jsx-arrays/JsxArraysExercicios.jsx'
-import Eventos from './pages/3-eventos/Eventos.jsx';
-import Componentes from './pages/4-componentes/Componentes.jsx';
-import Propriedades from './pages/5-propriedades/Propriedades.jsx';
+import JsxConceitos from './pages/1.1-jsx-conceitos/JsxConceitos.jsx'
+import JsxConceitosExercicios from './pages/1.1-jsx-conceitos/JsxConceitosExercicios.jsx'
+import JsxArrays from './pages/1.2-jsx-arrays/JsxArrays.jsx'
+import JsxArraysExercicio from './pages/1.2-jsx-arrays/JsxArraysExercicios.jsx'
+import Eventos from './pages/1.3-eventos/Eventos.jsx';
+import Componentes from './pages/1.4-componentes/Componentes.jsx';
+import Propriedades from './pages/1.5-propriedades/Propriedades.jsx';
 
 const root = document.getElementById('root');
 
