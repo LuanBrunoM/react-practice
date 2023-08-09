8475f5460b254ddfd4214bfdf677369d05e65f26
Luan Bruno <luanbrunomelo03@gmail.com>
2023-08-09 00:45:03 -0300

    update

--- src/pages/3.1-input/App.jsx
+++ src/pages/3.1-input/App.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 
 const App = () => {
+  const [form, setForm] = React.useState({
+    nome: '',
+    email: ''
+  })
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(nome, email);
+  }
+
+  function handleChange({ target }) {
+    const { id, value } = target;
+    console.log(id, value);
+    setForm({ ...form, [id]: value });
+  }
+
   return (
-    <div>App</div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="nome">Nome</label>
+      <input
+        type="text"
+        id="nome"
+        value={form.nome}
+        onChange={handleChange}
+      />
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        value={form.email}
+        onChange={handleChange}
+      />
+      <button>Enviar</button>
+    </form>
+  );
+};
+
+
 
 export default App
