4867effc5a20a2b2a282b2eb02ab3b9e83bbd093
Luan Bruno <luanbrunomelo03@gmail.com>
2023-08-29 18:05:08 -0300

    radio update

--- src/pages/3.4-radio/App.jsx
+++ src/pages/3.4-radio/App.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  const [produto, setProduto] = React.useState('');
+  const [cor, setCor] = React.useState('');
+
+  function handleChange({ target }) {
+    setProduto(target.value);
+  }
+
+  return <form>
+    <h2>Cores</h2>
+    {cor}
+    <label>
+      <input
+        type="radio"
+        onChange={({ target }) => setCor(target.value)}
+        checked={cor === 'amarelo'}
+        value="amarelo"
+      />
+      Amarelo
+    </label>
+    <label>
+      <input
+        type="radio"
+        onChange={({ target }) => setCor(target.value)}
+        checked={cor === 'azul'}
+        value="azul"
+      />
+      Azul
+    </label>
+
+    <h2>Produtos</h2>
+    {produto}
+    <label>
+      <input
+        type="radio"
+        onChange={handleChange}
+        checked={produto === 'smartphone'}
+        value="smartphone"
+      />
+      Smartphone
+    </label>
+    <label>
+      <input
+        type="radio"
+        onChange={handleChange}
+        checked={produto === 'notebook'}
+        value="notebook"
+      />
+      Notebook
+    </label>
+  </form>
 }
 
 export default App
